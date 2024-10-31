4c9d793fe62bf80152f212115b5115d850551a37
ibifaa ibisaki aruoture <ibifaaibisakiaruoture@gmail.com>
2024-10-31 17:47:14 +0100

    uninstalled disable react dev tool

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-if(ProcessingInstruction.env.NODE_ENV === 'production') disableReactDevTools()
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
